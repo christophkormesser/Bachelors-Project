8318ac489096b6a9122a5865cc4ab889c88699c4
Christoph Kormesser <christoph.kormesser@gmail.com>
2023-12-05 11:11:53 +0100

    initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Welcome to my project": "Christoph Kormesser",
+            "Docs:": "http://localhost:8001" + app.docs_url}
+
+@app.get("/action")
+def action(request: Request):
+
+    return {"Request from " + request.client.host + " went through!"}
