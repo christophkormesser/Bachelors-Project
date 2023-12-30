4d64c61f66ab22f12a239b93c7bd08ba3748bda3
Christoph Kormesser <christoph.kormesser@gmail.com>
2023-12-30 20:28:35 +0100

    adds fastapi2 heartbeat

--- app/main.py
+++ app/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI, Request
+import requests
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = FastAPI()
 
+def heartbeat():
+    # Replace with the URL of the service you want to call
+    url = "http://fastapi2:8001/action"
+    response = requests.get(url)
+    # You can log the response or perform other actions as needed
+    print(f"Heartbeat response: {response.status_code}")
+
+# Scheduler setup
+scheduler = BackgroundScheduler()
+scheduler.add_job(heartbeat, 'interval', minutes=1)
+scheduler.start()
+
+@app.on_event("shutdown")
+def shutdown_event():
+    scheduler.shutdown()
 
 @app.get("/")
 def read_root():
     return {"Welcome to my project": "Christoph Kormesser",
-            "Docs:": "http://servicename:8001" + app.docs_url}
+            "Docs:": "http://<servicename>:8001" + app.docs_url}
 
 @app.get("/action")
 def action(request: Request):
-
     return {"Request from " + request.client.host + " went through!"}
