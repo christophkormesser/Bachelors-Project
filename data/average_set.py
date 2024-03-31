d5593ae6e94ee0117d3b8e0c0b4c3e7a6c065cd8
Christoph Kormesser <christoph.kormesser@gmail.com>
2024-03-31 20:42:55 +0200

    adds min/max duration & timestamps

--- data/average_set.py
+++ data/average_set.py
@@ -5,27 +5,45 @@ def load_data(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
-file_path = 'data/processed/istio-mtls-traces-p.json'
+file_path = 'data/processed/traces_1711907323418568_1711907923418568.json'
 data = load_data(file_path)
 
-# Calculate the average duration and response size for each source/destination combination
+# Calculate the average duration, response size and track min/max durations for each source/destination combination
 average_metrics = {}
 for entry in data:
     key = (entry['source'], entry['destination'])
     if key not in average_metrics:
-        average_metrics[key] = {'total_duration': 0, 'total_response_size': 0, 'count': 0}
+        average_metrics[key] = {
+            'total_duration': 0, 
+            'total_response_size': 0, 
+            'count': 0,
+            'min_duration': float('inf'),
+            'timestamp_min_duration': float,
+            'max_duration': float('-inf'),
+            'timestamp_max_duration': float
+            }
     average_metrics[key]['total_duration'] += entry['duration']
     average_metrics[key]['total_response_size'] += entry['response_size']
     average_metrics[key]['count'] += 1
-
-# Compute the average duration and response size, and store it in a new list
+    # Update min and max durations
+    if entry['duration'] < average_metrics[key]['min_duration']:
+        average_metrics[key]['min_duration'] = entry['duration']
+        average_metrics[key]['timestamp_min_duration'] = entry['startTime']
+    if entry['duration'] > average_metrics[key]['max_duration']:
+        average_metrics[key]['max_duration'] = entry['duration']
+        average_metrics[key]['timestamp_max_duration'] = entry['startTime']
+# Compute the average duration, response size and  and store it in a new list
 averaged_data = []
 for key, value in average_metrics.items():
     averaged_data.append({
         'source': key[0],
         'destination': key[1],
         'average_duration': value['total_duration'] / value['count'],
-        'average_response_size': value['total_response_size'] / value['count']
+        'average_response_size': value['total_response_size'] / value['count'],
+        'min_duration': value['min_duration'],
+        'timestamp_min_duration': value['timestamp_min_duration'],
+        'max_duration': value['max_duration'],
+        'timestamp_max_duration': value['timestamp_max_duration']
     })
 
 print(averaged_data)
@@ -42,6 +60,9 @@ for entry in data:
 status_code_consistency = {key: len(codes) == 1 for key, codes in status_codes.items()}
 print(status_code_consistency)
 
+print("Writing to file...")
 
-with open('data/processed/istio-mtls-traces-average-p.json', 'w', encoding='utf-8') as f:
+with open('data/averages/traces_1711907323418568_1711907923418568.json', 'w', encoding='utf-8') as f:
     json.dump(averaged_data, f, ensure_ascii=False, indent=4)
+
+print("Done.")
