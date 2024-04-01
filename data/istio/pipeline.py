437dc9e1cac68bdd02a409ae50ba836ce6528d80
Christoph Kormesser <christoph.kormesser@gmail.com>
2024-04-01 23:02:35 +0200

    adds overall averages for metrics #2

--- data/istio/pipeline.py
+++ data/istio/pipeline.py
@@ -4,49 +4,54 @@ from fetch_traces import fetch_traces
 from extract_traces import extract_traces
 from average_set import calc_averages
 from get_first_and_last_ts import get_trace_interval
+from average_node_metrics import calculate_averages
 import logging
 from datetime import datetime
 
-logger = logging.getLogger(__name__)
-temp_name = f"data/istio/logs/pipeline_{datetime.now().timestamp()}.log"
-logging.basicConfig(filename=temp_name, encoding="utf-8", level=logging.DEBUG)
-
 def execute_pipeline(raw_file=None):
+
+    logger = logging.getLogger(__name__)
+    temp_name = f"data/istio/logs/pipeline_{int(datetime.now().timestamp())}.log"
+    logging.basicConfig(filename=temp_name, encoding="utf-8", level=logging.DEBUG)
+
     logger.info("Starting pipeline...")
 
     if raw_file == None:
-        #start metric gathering
+        # start metric gathering
         start_time, end_time = get_node_metrics()
         logger.info("Finished metric gathering.")
         
-        #scale down deployments
+        # scale down deployments
         logger.info("Scaling down applications...")
         deployments_to_scale_down = ['app1', 'app2', 'app3']
         for deployment_name in deployments_to_scale_down:
             scale_down_deployment(deployment_name)
         logger.info("Finished scaling down applications.")
 
-        #fetch traces
+        # fetch traces
         logger.info("Attempting to fetch traces...")
         fetch_traces(start_time=start_time, end_time=end_time)
         logger.info("Finished fetching traces.")
 
-    #extract traces
+    # extract traces
     logger.info("Extracting traces...")
     if raw_file == None:
         raw_file = f"data/istio/traces/raw/traces_{start_time}.json"
     extract_traces(raw_file=raw_file)
     logger.info("Finished extracting traces.")
 
-    #get averages
+    # get averages
     logger.info("Calculating average, min & max duration...")
     processed_file = raw_file.replace("raw", "processed")
     calc_averages(processed_file=processed_file)
     logger.info("Finished calculations.")
 
-    #get trace interval
+    # get trace interval
     logger.info("Getting actual trace interval time...")
     get_trace_interval(processed_file=processed_file)
+
+    # calculate node metric averages considering trace data time frame
+    calculate_averages(f"data/istio/metrics/node_metrics-{start_time}.json", processed_file)
 
     logger.info("Finished pipeline.")
 
