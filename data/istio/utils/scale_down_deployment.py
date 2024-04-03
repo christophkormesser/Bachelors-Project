1269d043b52b9e0b7bb008c730d43c38485f22d3
Christoph Kormesser <christoph.kormesser@gmail.com>
2024-04-03 19:17:43 +0200

    adds prefix

--- data/istio/utils/scale_down_deployment.py
+++ data/istio/utils/scale_down_deployment.py
@@ -1,9 +1,13 @@
 from kubernetes import client
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 import logging
 
+load_dotenv()
+
 logger = logging.getLogger(__name__)
-temp_name = f"data/istio/logs/scale_down_{int(datetime.now().timestamp())}.log"
+temp_name = f"data/istio/logs/{os.getenv('PREFIX')}_scale_down_{int(datetime.now().timestamp())}.log"
 logging.basicConfig(filename=temp_name, encoding="utf-8", level=logging.DEBUG)
 
 def scale_down_deployment(deployment_name, namespace='default'):
